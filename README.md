f62137b8bf656d521ce94df08d3e98264731362a
jorivanc <jorivanc@hotmail.com>
2019-06-07 17:28:44 -0700

    Last Project Update before sending for reviewing

--- README.md
+++ README.md
@@ -8,17 +8,27 @@ Browser-based card matching game (also known as Concentration)
 2. Open another card. If they match, then return to step 1.  Otherwise remember the cards previously opened and try to find pairs of matching cards.
 3. Now do this until all the cards have been opened in the minimum amount of moves and time.
 
+### Using the keyboard.
+Keyboard accessibility has been implemented using the following keys.
+- To move through the cards:
+  * To the right card press the Right Arrow (→) or Tab ( ↹ ) key.
+  * To the left card press the Left Arrow (←) or Shift + Tab ( ⇧ + ↹ ) keys.
+- To flip/open a card press Enter ( ↵ )
+- To restart the game press the R key.
+- To continue after winning a game and close/exit the popup modal window press Enter ( ↵ )
+
 ## Future Improvements.
 - Implement a _Settings_ option:
   * Add the option to set a countdown timer. (just as a challenge for the player)
   * Let the user set a difficulty level (pick the number of cards). See **Note.**
-- Implement keyboard accessibility.
-- Improve the way the rating is calculated. Right now the star rating is calculated solely on the amount of moves. It would be better to also take into account the time spent solving the game.
+- ~~Implement keyboard accessibility~~. : keyboard accessibility has been implemented but code optimization could be improved.
+- Improve the way the rating is calculated. Right now the (stars) rating is calculated solely based on the amount of moves. It would be better to also take into account the time spent solving the game.
 - Implement a Score Panel: Allow users to input their names and record their scores and display a panel with those result ordered from best to ..... not so good.
+- Improve accessibility issues: I tested playing the Memory Game using ChromeVox screen reader, but the reader reads the symbol description of each card. I tried changing the alt attribute of each card to cardN where N is a distinct number assigned to each card but the reader still reads the symbol descriptions.
 
 
 #### Note
-The function loadCardsToHtml allows to load a series/array of cards of any length to the board. So it would be possible to allow players to select the amount of cards (set the difficulty) to play, as long as there are an even number of cards.
+The function loadCardsToHtml (in the file scripts.js) allows to load a series/array of cards of any length to the board. So it would be possible to allow players to select the amount of cards (set the difficulty) to play, as long as there are an even number of cards.
 ```
 function loadCardsToHtml(series){
   let msgHTML = "";
@@ -28,11 +38,13 @@ function loadCardsToHtml(series){
   board.innerHTML = msgHTML;
 }//end loadCardsToHtml
 ```
-To test this feature simply comment out 2n cards from scrips.js
+To test this feature simply comment out 2n (an even amount) cards from the file scrips.js
 ```
 let cards = [];
 cards[0] = new card('🍺', 'beer');
 cards[1] = new card('🍺', 'beer');
+// cards[2] = new card('☘', 'clover');
+// cards[3] = new card('☘', 'clover');
 // 2n cards commented out
 ...
 ```
@@ -45,5 +57,6 @@ cards[1] = new card('🍺', 'beer');
 6. Moves Counter
 7. Styling
 8. Responsiveness
-9. Comments
-10. README
+9. Keyboard Accessibility
+10. Comments
+11. README
--- js/scripts.js
+++ js/scripts.js
@@ -1,147 +1,271 @@
 // CARD Object declaration
-  function card(symbol,symbolDescription){
-    this.symbol = symbol;
-    this.symbolDescription = symbolDescription;
-  }
+function card(symbol,symbolDescription){
+  this.symbol = symbol;
+  this.symbolDescription = symbolDescription;
+}
 
 // SHUFFLES AN ARRAY (of cards) using Fisher–Yates algorithm
-  function shuffleArray(series){
-    for (let i = 0; i < series.length; i++){
+function shuffleArray(series){
+  for (let i = 0; i < series.length; i++){
     let randomPos = Math.floor(Math.random()*series.length);
     let temp = series[randomPos];
     series[randomPos] = series[series.length-i-1];
     series[series.length-i-1] = temp;
-    }
-  }//enf funcion shuffleArray
+  }
+}//enf funcion shuffleArray
 
 // LOADS CARDS TO THE BOARD
-  function loadCardsToHtml(series){
-    let msgHTML = "";
-    for ( let card of series) {
-      msgHTML += `<li data-type="${card.symbolDescription}" class="card face__down" alt="${card.symbolDescription}"> ${card.symbol} </li> \n`;
-    }
-    board.innerHTML = msgHTML;
-  }//end loadCardsToHtml
+function loadCardsToHtml(series){
+  let tabIndex = 1;
+  let msgHTML = "";
+  for ( let card of series) {
+    msgHTML += `<li data-type="${card.symbolDescription}" class="card face__down" alt="card${tabIndex}" tabIndex="${tabIndex++}"> ${card.symbol} </li> \n`;
+  }
+  board.innerHTML = msgHTML;
+}//end loadCardsToHtml
 
 // HIDES AND RESETS CONTENT IN WIN POPUP WINDOW after wining a game
-  function hideWinPopupWindow(){
-    // disable reset game button while popup window is being displayed
-    const reset = document.getElementById("reset");
-    reset.classList.toggle("disable");
-    // hides popup window
-    const wmContainer = document.getElementById("win__message__popup");
-    wmContainer.classList.toggle("visible");
-    // resets popup window content to initial values
-    const ratings = document.getElementById("wm__rating");
-    ratings.textContent = "Rating: ";
-    const wmMoves = document.getElementById("wm__moves");
-    wmMoves.textContent = "Moves: ";
-    const wmTime = document.getElementById("wm__time");
-    wmTime.textContent = "Time: ";
-    resetGame();
-  }
+function hideWinPopupWindow(){
+  // disable reset game button while popup window is being displayed
+  const reset = document.getElementById("reset");
+  reset.classList.toggle("disable");
+  // hides popup window
+  const wmContainer = document.getElementById("win__message__popup");
+  wmContainer.classList.toggle("visible");
+  // resets popup window content to initial values
+  const ratings = document.getElementById("wm__rating");
+  ratings.textContent = "Rating: ";
+  const wmMoves = document.getElementById("wm__moves");
+  wmMoves.textContent = "Moves: ";
+  const wmTime = document.getElementById("wm__time");
+  wmTime.textContent = "Time: ";
+  // once the popup window has been closed the game will be restarted
+  resetGame();
+}
 
-  function displayWinMessage(){
-      const reset = document.getElementById("reset");
-      reset.classList.toggle("disable");
-      const ratings = document.getElementById("rating");
-      const wmRating = document.getElementById("wm__rating");
-      wmRating.textContent = wmRating.textContent + ratings.textContent;
-      const wmMoves = document.getElementById("wm__moves");
-      wmMoves.textContent += counterMoves;
-      const wmTime = document.getElementById("wm__time");
-      const timer = document.getElementById("timer");
-      wmTime.textContent = wmTime.textContent + timer.textContent;
-      const wmContinueButton = document.getElementById("continue__button");
-      wmContinueButton.addEventListener('click', hideWinPopupWindow);
-      const wmContainer = document.getElementById("win__message__popup");
-      wmContainer.classList.toggle("visible");
-  }
+// UDATES POPUP WINDOW WITH CURRENT SCORES AND DISPLAYS IT
+function displayWinMessage(){
+  let cButton = document.getElementById("continue__button");
+  indexCardInFocus = 1;
+  cButton.setAttribute(`tabIndex`,`${indexCardInFocus}`);
+  window.setTimeout(function () { document.getElementById('continue__button').focus(); }, 0);
+  const reset = document.getElementById("reset");
+  reset.classList.toggle("disable");
+  const ratings = document.getElementById("rating");
+  const wmRating = document.getElementById("wm__rating");
+  wmRating.textContent = wmRating.textContent + ratings.textContent;
+  const wmMoves = document.getElementById("wm__moves");
+  wmMoves.textContent += counterMoves;
+  const wmTime = document.getElementById("wm__time");
+  const timer = document.getElementById("timer");
+  wmTime.textContent = wmTime.textContent + timer.textContent;
+  const wmContinueButton = document.getElementById("continue__button");
+  wmContinueButton.addEventListener('click', hideWinPopupWindow);
+  const wmContainer = document.getElementById("win__message__popup");
+  wmContainer.classList.toggle("visible");
+}
 
-  function checkWinGame(){
-    if (matchedCards == cards.length/2){
-      win = true;
-      displayWinMessage();
-    }
+// VERIFIES IF PLAYER WINS by checking that all cards are open and matched
+function checkWinGame(){
+  if (matchedCards == cards.length/2){
+    win = true;
+    displayWinMessage();
   }
+}
 
-  function updateTimerOnScreen(){
-    seconds++;
-    let stringTime = "";
-    if(seconds==60){
-        minutes++;
-        seconds=0;
-      }
-    // display seconds and minutes in the format 00:00 mm:ss
-    (minutes<10)? stringTime += "0"+minutes: stringTime += minutes;
-    (seconds<10)? stringTime += ":0"+seconds: stringTime += ":"+seconds;
-    const timerDisplay = document.getElementById('timer');
-    timerDisplay.textContent = stringTime;
-    if (win){
-      clearInterval(timer);
-      }
+// FORMATS AND DISPLAYS TIME ON SCREEN EVERY SECOND
+function updateTimerOnScreen(){
+  seconds++;
+  let stringTime = "";
+  if(seconds==60){
+    minutes++;
+    seconds=0;
   }
+  // display seconds and minutes in the format 00:00 mm:ss
+  (minutes<10)? stringTime += "0"+minutes: stringTime += minutes;
+  (seconds<10)? stringTime += ":0"+seconds: stringTime += ":"+seconds;
+  const timerDisplay = document.getElementById('timer');
+  timerDisplay.textContent = stringTime;
+  if (win){
+    clearInterval(timer);
+  }
+}
 
-  function startTimer(){
-     // setTimeOut could have been used, but it would have to be recursively called, which can me done automatically with setInterval
+// STARTS THE TIMER as soon as player has opened two cards (has attempted one match, in other words has made a move)
+function startTimer(){
+  // setTimeOut could have been used, but it would have to be recursively called, which can me done automatically with setInterval
   timer = setInterval(updateTimerOnScreen,1000);
-  }
+}
 
-  function updateMovesOnScreen(){
-    counterMoves++;
-    if (counterMoves==1){
-      startTimer() ;
-      }
-    const moves = document.getElementById('moves');
-    moves.textContent = counterMoves;
+function updateMovesOnScreen(){
+  counterMoves++;
+  if (counterMoves==1){
+    startTimer() ;
   }
+  const moves = document.getElementById('moves');
+  moves.textContent = counterMoves;
+}
 
-  function updateRatingsOnScreen(){
-    const rating = document.getElementById('rating');
-    if (counterMoves <= (cards.length - cards.length/4)){
-      rating.textContent = "⭐ ⭐ ⭐";
-    }
-    if (counterMoves > (cards.length - cards.length/4) && (counterMoves <= cards.length)){
-      rating.textContent = "⭐ ⭐";
-    }
-    if (counterMoves > cards.length){
-      rating.textContent = "⭐";
-    }
+function updateRatingsOnScreen(){
+  const rating = document.getElementById('rating');
+  if (counterMoves <= (cards.length - cards.length/4)){
+    rating.textContent = "⭐ ⭐ ⭐";
   }
+  if (counterMoves > (cards.length - cards.length/4) && (counterMoves <= cards.length)){
+    rating.textContent = "⭐ ⭐";
+  }
+  if (counterMoves > cards.length){
+    rating.textContent = "⭐";
+  }
+}
 
-  // VERIFIES IF 2 CARDS MATCH (the 2 cards stored in cardsToMatch array. if they match they will be disable otherwise the will be placed facedown and ready to be clicked again)
-  function checkMatch(){
-    cardsToMatch[0].classList.toggle('show__Animation');
-    cardsToMatch[1].classList.toggle('show__Animation');
-    if(cardsToMatch[0].getAttribute("data-type")==cardsToMatch[1].getAttribute("data-type")){
-      // if the cards match then you will not ba able to click on them again
-      cardsToMatch[0].classList.toggle('disable');
-      cardsToMatch[1].classList.toggle('disable');
-      matchedCards++;
-    }else{
-      //if the cards didnt match the cards will be flipped face down and be ready to be clicked on again
-      cardsToMatch[0].classList.toggle('face__down');
-      cardsToMatch[1].classList.toggle('face__down');
-      cardsToMatch[0].classList.toggle('clicked');
-      cardsToMatch[1].classList.toggle('clicked');
+// VERIFIES IF 2 CARDS MATCH (the 2 cards stored in cardsToMatch array. if they match they will be disable otherwise the will be placed facedown and ready to be clicked again)
+function checkMatch(){
+  cardsToMatch[0].classList.toggle('show__Animation');
+  cardsToMatch[1].classList.toggle('show__Animation');
+  if(cardsToMatch[0].getAttribute("data-type")==cardsToMatch[1].getAttribute("data-type")){
+    // if the cards match then you will not ba able to click on them again
+    cardsToMatch[0].classList.toggle('disable');
+    cardsToMatch[1].classList.toggle('disable');
+    cardsToMatch[0].setAttribute('tabIndex','-1');
+    cardsToMatch[1].setAttribute('tabIndex','-1');
+    // if a match is found then the keyboard focus will jump to the next available card
+    if (matchedCards < Math.floor((cards.length/2)-1)){
+      jumpToNext();
+      document.querySelector(`li[tabIndex='${indexCardInFocus}']`).focus();
     }
-    //after it has been decided if the cards selected matched or not they will be eliminated from the array of cards to match
-    cardsToMatch.pop();
-    cardsToMatch.pop();
-    checkWinGame();
+    matchedCards++;
+  }else{
+    //if the cards didnt match the cards will be flipped face down and be ready to be clicked on again
+    cardsToMatch[0].classList.toggle('face__down');
+    cardsToMatch[1].classList.toggle('face__down');
+    cardsToMatch[0].classList.toggle('clicked');
+    cardsToMatch[1].classList.toggle('clicked');
+    cardsToMatch[0].setAttribute('tabIndex',`${clickedCard[1]}`);
   }
+  //after it has been decided if the cards selected matched or not they will be eliminated from the array of cards to match
+  cardsToMatch.pop();
+  cardsToMatch.pop();
+  checkWinGame();
+}
+
+// disable temporary the current selected card (so a user cannot select the same card twice) by setting tabIndex=-1 and jump to the next available card, but i need to storage the actual tabIndex and the card in case theres not a match ill return the card to its original values
+function focusOnNextCard(e){
+  clickedCard[0] = e.target;
+  clickedCard[1] = indexCardInFocus;
+  if (cardsToMatch.length == 0){
+    e.target.setAttribute('tabIndex','-1');
+    jumpToNext();
+    document.querySelector(`li[tabIndex='${indexCardInFocus}']`).focus();
+  }
+}
 
 function respondBoardClick(event){
-      //if the element in the board was a card (an <LI> element) and there were 2 or less cards selected then it will check if they match )
-      if (event.target.nodeName === 'LI' && (cardsToMatch.length < 2) ){
-        //when a card is clicked on, if the card was face up then ill turn it back face down and if it was face down then it will show it face up
-        //Im going to allow the user to pick the same card twice (put face up and again face down) but it will be counted as a move
-          event.target.classList.toggle('face__down');
-          event.target.classList.toggle("clicked");
-          // console.log(event.target.classList);
-          cardsToMatch.push(event.target);
-          // counterClicks++;
+  //if the element in the board was a card (an <LI> element) and there were 2 or less cards selected then it will check if they match )
+  if (event.target.nodeName === 'LI' && (cardsToMatch.length < 2) ){
+    //when a card is clicked on, if the card was face up then ill turn it back face down and if it was face down then it will show it face up
+    event.target.classList.toggle('face__down');
+    event.target.classList.toggle("clicked");
+    indexCardInFocus = event.target.getAttribute('tabIndex');
+    // if only one card has been opened, since the player cannot click/open the same card twice, it will be disabled and the focus will go to the next available card
+    if (cardsToMatch.length == 0){
+      focusOnNextCard(event);
+    }
+    cardsToMatch.push(event.target);
+    //if two cards have been selected then we will check if they match
+    if (cardsToMatch.length == 2){
+      cardsToMatch[0].classList.toggle('show__Animation');
+      cardsToMatch[1].classList.toggle('show__Animation');
+      setTimeout(checkMatch,800);
+      // once two cards have been clicked the number of moves must be updated and depending on the number of moves the ratings will change
+      updateMovesOnScreen();
+      updateRatingsOnScreen();
+    }
+  }
+}
 
+// MOVES FOCUS TO THE PREVIOUS AVAILABLE CARD
+function jumpToPrevious(){
+  if(indexCardInFocus==1){
+    indexCardInFocus=16;
+  }else if(indexCardInFocus!=0){
+    indexCardInFocus--;
+  }
+    if (indexCardInFocus==0){
+      indexCardInFocus=1;
+    }
+    while(!document.querySelector(`li[tabIndex='${indexCardInFocus}']`)){
+      if (indexCardInFocus==1){
+        indexCardInFocus=17;
+      }
+      indexCardInFocus--;
+    }
+}
+
+// MOVES FOCUS TO THE NEXT AVAILABLE CARD
+function jumpToNext(){
+  if(indexCardInFocus==16){
+    indexCardInFocus=1;
+  }else{
+    indexCardInFocus++;
+  }
+  if (indexCardInFocus==0){
+    indexCardInFocus=1;
+  }
+  while(!document.querySelector(`li[tabIndex='${indexCardInFocus}']`)){
+    if (indexCardInFocus==16){
+      indexCardInFocus=0;
+    }
+    indexCardInFocus++;
+  }
+}
+
+function respondBoardKey(event){
+  if (win){
+    if(event.keyCode === 13){
+      hideWinPopupWindow();
+      resetGame();
+    }
+    return;
+  }
+  if (event.shiftKey && event.keyCode === 9) {
+    jumpToPrevious();   // do jumpToPrevious twice because i didnt know how to put it on the switch statement. since a tab key has been pressed (keycode==9) then it will go forward once its read on the switch
+    jumpToPrevious();
+    document.querySelector(`li[tabIndex='${indexCardInFocus}']`).focus();
+  }
+  switch (event.keyCode) {
+    case 82:            // R key
+      resetGame();
+      break;
+    case 37:            // Left Arrow key
+      jumpToPrevious();
+      document.querySelector(`li[tabIndex='${indexCardInFocus}']`).focus();
+      break;
+    case 9:            // Tab key
+      event.preventDefault();
+      jumpToNext();
+      document.querySelector(`li[tabIndex='${indexCardInFocus}']`).focus();
+      break;
+    case 39:            // Right Arrow key
+      jumpToNext();
+      document.querySelector(`li[tabIndex='${indexCardInFocus}']`).focus();
+      break;
+    case 13:            // Enter key
+      if (indexCardInFocus==0){
+        break;
+      }else{
+        let cardInFocus = document.querySelector(`li[tabIndex='${indexCardInFocus}']`);
+        //When only one card has been selected then the focus will go to the next available card (since the same card cannot be clicked/opened twice)
+        if (cardsToMatch.length == 0){
+          focusOnNextCard(event);
+        }
+        //if the element in the board was a card (an <LI> element) and there were 2 or less cards selected then it will check if they match )
+        if (cardsToMatch.length < 2){
+          //when a card is clicked on, if the card was face up then ill turn it back face down and if it was face down then it will show it face up
+          //Im going to allow the user to pick the same card twice (put face up and again face down) but it will be counted as a move
+          cardInFocus.classList.toggle('face__down');
+          cardInFocus.classList.toggle("clicked");
+          cardsToMatch.push(cardInFocus);
           //if two cards have been selected then we will check if they match
           if (cardsToMatch.length == 2){
             cardsToMatch[0].classList.toggle('show__Animation');
@@ -150,11 +274,14 @@ function respondBoardClick(event){
             // once two cards have been clicked the number of moves must be updated and depending on the number of moves the ratings will change
             updateMovesOnScreen();
             updateRatingsOnScreen();
-            }
+          }
         }
+      }
     }
+}
 
 function resetGame(){
+  indexCardInFocus = 0;
   counterMoves = 0;
   matchedCards = 0;
   clearInterval(timer);
@@ -166,6 +293,7 @@ function resetGame(){
   moves.textContent = counterMoves;
   const timerDisplay = document.getElementById('timer');
   timerDisplay.textContent = "00:00";
+  document.getElementById("continue__button").setAttribute('tabIndex','-1');
   play();
 }
 
@@ -174,6 +302,7 @@ function play(){
   loadCardsToHtml(cards);
   updateRatingsOnScreen();
   board.addEventListener('click', respondBoardClick);
+  document.addEventListener('keydown', respondBoardKey);
   const reset = document.getElementById("reset");
   reset.addEventListener('click', resetGame);
 }
@@ -183,6 +312,8 @@ let minutes = 0;
 let win = false;
 let timer;
 let cardsToMatch = [];
+let clickedCard = [];
+let indexCardInFocus = 0;
 let counterMoves = 0;
 let matchedCards = 0;
 let cards = [];
